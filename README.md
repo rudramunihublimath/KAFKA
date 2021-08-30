4529c6cda403f0a51fc5d5a79d64ef0851f696e3
rudra.hublimath <rudra.hublimath@gmail.com>
2021-08-30 15:30:56 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+URL : http://localhost:9090/addCustomer
+Sample :
+
+[
+{
+"customerId":101,
+"customerName":"R1",
+"customerEmail":"a@gmail.com"
+},
+{
+"customerId":102,
+"customerName":"R2",
+"customerEmail":"b@gmail.com"
+}
+]
---
+++ src/main/java/com/rph/SpringBootApacheKafkaPocApplication.java
@@ -0,0 +1,17 @@
+package com.rph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author
+ *
+ */
+@SpringBootApplication
+public class SpringBootApacheKafkaPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApacheKafkaPocApplication.class, args);
+	}
+}
